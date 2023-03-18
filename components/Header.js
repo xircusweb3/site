a8a07ee37c7859bf1704d100e0cf896bd6e5cc26
Mikey Molina <development@xircus.app>
2023-03-18 13:22:42 +0800

    Added russian language

--- components/Header.js
+++ components/Header.js
@@ -53,6 +53,7 @@ const ChangeLocale = (props) => (
     {...props}>
     <option value="en">English</option>
     <option value="zh">中文</option>
+    <option value="ru">Russian</option>    
   </Select>
 )
 
--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   i18n: {
-    locales: ['en', 'zh'],
+    locales: ['en', 'zh', 'ru'],
     defaultLocale: 'en'
   },  
   reactStrictMode: true,
